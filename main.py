0087b82ce09a9765d974a49269f2e3b6d40d745c
Sönke Schröder <soenke.schroeder@iu-study.org>
2026-09-12 18:11:59 +0200

    Textbereinigung grundlegend fertig. Datei- und Spaltenauswahl wird später ergänzt.

--- main.py
+++ main.py
@@ -10,15 +10,90 @@
 # - Ergebnisse vergleichen und auswerten
 # ===========================================================
 
+
 # 1 - Biblitheken importieren
+
+    #Abhängigkeiten:
+    #    pandas
+    #    scikit-learn
+    #    nltk
+
+import string
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download("punkt_tab")
+
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import TruncatedSVD, LatentDirichletAllocation
+
+
 
 # 2 - Datensatz laden
 
     # CSV einlesen
+datafile = pd.read_csv("municipal_training_set_1100.csv")
+'''
+print("Erste Zeilen:")
+print(datafile.head())
+print("Spalten:")
+print(datafile.columns)
+print("Größe:")
+print(datafile.shape)
+print("Null-Felder")
+print(datafile.isnull().sum())
+'''
     # relevante Spalten auswählen
+        # -> issue_description
+# Spalte mit Beschwerdetexten auswählen:
+#texts = datafile["issue_description"]
+# Erste Beschwerdetexte anzeigen:
+#print("Beispieltexte:")
+#print(texts.head())
+
 
 # 3 - Texte bereinigen
 
+#Stoppwörter und Lemmatizer vorbereiten
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+def clean_text(text):
+
+    '''
+    Bereinigt Text. Umwandlung in Kleinbuchstaben, Satzzeichen und Stoppwörter entfernen und lemmatieren.
+    :param text: CSV-Spalte mit Beschwerden
+    :return: CSV-Spalte mit bereinigten Beschwerden
+    '''
+
+    # In Kleinbuchstaben umwandeln
+    text = text.lower()
+
+    # Satzzeichen entfernen
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # Text in einzelne Wörter zerlegen
+    tokens = word_tokenize(text)
+
+    # Stopwörter entfernen
+    tokens = [word for word in tokens if word not in stop_words]
+
+    # Wörter lemmatisieren
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+
+    #Token
+    return " ".join(tokens)
+
+
+datafile["clean_description"] = datafile["issue_description"].apply(clean_text)
+#print(datafile[["issue_description","clean_description"]].head())
     # Text in Kleinbuchstaben umwandeln
     # Satzzeichen entfernen
     # Text tokenisieren

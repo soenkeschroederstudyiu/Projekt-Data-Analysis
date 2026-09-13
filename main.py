1775fde29d4c53045e3d64ba39c06af6147879d1
Sönke Schröder <soenke.schroeder@iu-study.org>
2026-09-13 22:11:38 +0200

    Code-Lesbarkeit und Usability verbessert print_topics überarbeitet Ausgabe verschlankt

--- main.py
+++ main.py
@@ -10,68 +10,52 @@
 # - Ergebnisse vergleichen und auswerten
 # ===========================================================
 
+#Abhängigkeiten:
+#    pandas
+#    scikit-learn
+#    nltk
+#    gensim
+#    mathplotlib
 
-# 1 - Biblitheken importieren
-
-    #Abhängigkeiten:
-    #    pandas
-    #    scikit-learn
-    #    nltk
-    #    gensim
-    #    mathplot
+# =========================================================
+# Importe:
+# ==========================================================
 
 import string
 import pandas as pd
+
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download("punkt_tab")
+nltk.download("stopwords", quiet=True)
+nltk.download("punkt", quiet=True)
+nltk.download("wordnet", quiet=True)
+nltk.download("punkt_tab", quiet=True)
 
-import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import TruncatedSVD, LatentDirichletAllocation
 
-from gensim.corpora import Dictionary, dictionary
+from gensim.corpora import Dictionary
 from gensim.models import CoherenceModel
 
 import matplotlib.pyplot as plt
 
-# 2 - Datensatz laden
+# ==========================================================
+# Konstanten:
+# ==========================================================
 
-    # CSV einlesen
-datafile = pd.read_csv("municipal_training_set_1100.csv")
-'''
-print("Erste Zeilen:")
-print(datafile.head())
-print("Spalten:")
-print(datafile.columns)
-print("Größe:")
-print(datafile.shape)
-print("Null-Felder")
-print(datafile.isnull().sum())
-'''
-    # relevante Spalten auswählen
-        # -> issue_description
-# Spalte mit Beschwerdetexten auswählen:
-#texts = datafile["issue_description"]
-# Erste Beschwerdetexte anzeigen:
-#print("Beispieltexte:")
-#print(texts.head())
+NUMBER_OF_WORDS = 10
+DATA_PATH = "municipal_training_set_1100.csv"
+TOPIC_NUMBERS=range(2, 16)
+PRINT_SCORES = False # Detailausgabe bei der berechnung der einzelnen Coherence Scores
 
-
-# 3 - Texte bereinigen
-
-#Stoppwörter und Lemmatizer vorbereiten
-stop_words = set(stopwords.words('english'))
-lemmatizer = WordNetLemmatizer()
+# ==========================================================
+# Funktionen
+# ==========================================================
 
 def clean_text(text):
-
     '''
     Bereinigt Text. Umwandlung in Kleinbuchstaben, Satzzeichen und Stoppwörter entfernen und lemmatieren.
     :param text: CSV-Spalte mit Beschwerden
@@ -96,89 +80,22 @@ def clean_text(text):
     #Token
     return " ".join(tokens)
 
-
-datafile["clean_description"] = datafile["issue_description"].apply(clean_text)
-#print(datafile[["issue_description","clean_description"]].head())
-
-# 4 - Texte vektorisieren
-
-    # BOW
-bow_vectorizer = CountVectorizer()
-bow_matrix = bow_vectorizer.fit_transform(datafile["clean_description"])
-
-#print("BOW: ",bow_matrix)
-#print(bow_vectorizer.get_feature_names_out())
-#print(bow_matrix.shape)
-
-    # TF-IDF
-tfidf_vectorizer = TfidfVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform(datafile["clean_description"])
-
-#print("TF-IDF: ", tfidf_matrix)
-#print(tfidf_matrix.shape)
-#print(tfidf_vectorizer.get_feature_names_out())
-
-
-# 5 - BOW und TF-IDF vergleichen
-#print("BOW-MAtrix")
-#bow_df = pd.DataFrame(bow_matrix.toarray(),columns=bow_vectorizer.get_feature_names_out())
-#print(bow_df)
-#print("Tfidf-Matrix")
-#tfidf_df = pd.DataFrame(tfidf_matrix.toarray(),columns=tfidf_vectorizer.get_feature_names_out())
-#print(tfidf_df)
-#print("Häufigste Wörter nach BOW:")
-#bow_word_counter = bow_df.sum().sort_values(ascending=False)
-#print(bow_word_counter.head(20))
-#print("Höchste TF-IDF-Werte:")
-#tfidf_word_scores = tfidf_df.sum().sort_values(ascending=False)
-#print(tfidf_word_scores.head(20))
-
-# 6 - Themen mit extrahieren
-number_of_topics = 5
-number_of_words = 10
-
-def print_topics(model, feature_names, number_words):
+def print_topics(topics):
     '''
-
-    :param model: LDA- oder LSA-Model
-    :param feature_names: Liste der Themen
-    :param number_words: Anzahl der Worte pro Thema
-    :return:
+    Gibt Themen lesbar aus.
+    :param topics: Themen, die angezeigt werden sollen
     '''
-    for topic_index, topic in enumerate(model.components_):
-        top_indices = topic.argsort()[-number_words:][::-1]
-        top_words = [feature_names[i] for i in top_indices]
-        print(f"{topic_index + 1}: {', '.join(top_words)}")
+    for index, topic in enumerate(topics):
+        print(f"Thema {index + 1}: {', '.join(topic)}")
 
-
-
-    # LSA
-
-lsa_model = TruncatedSVD(n_components=number_of_topics, random_state=42) # random_state für reproduzierbare Ergebnisse festgelegt
-lsa_model.fit(tfidf_matrix)
-
-#print("LSA:")
-#print_topics(lsa_model, tfidf_vectorizer.get_feature_names_out(), number_of_words)
-
-    # LDA
-
-lda_model = LatentDirichletAllocation(n_components=number_of_topics, random_state=42) # random_state für reproduzierbare Ergebnisse festgelegt
-lda_model.fit(bow_matrix)
-
-#print("LDA:")
-#print_topics(lda_model, bow_vectorizer.get_feature_names_out(), number_of_words)
-
-
-
-# 7 - Themenanzahl mit Coherence Score bestimmen
 def get_topics_coherence(model, feature_names, clean_texts, number_words):
     '''
-
+    Extrahiert Themen aus den bereinigten Texten und berechne die Kohärenz
     :param model: LDA- oder LSA-Model
     :param feature_names: Liste der Themen
     :param clean_texts: bereinigte Texte
     :param number_words: Anzahl der Worte pro Thema
-    :return:
+    :return: topics, coherence_score
     '''
     topics = []
     #wichtigste Wörter je Thema bestimmen
@@ -200,59 +117,115 @@ def get_topics_coherence(model, feature_names, clean_texts, number_words):
         texts=tokenized_texts,
         dictionary=gensim_dictionary,
         coherence="c_v",
-        processes=1 # Verwendung mehrerer Prozesse kann unter Windows zu Problemen in der Bibliothek gensim führen.
+        processes=1  # Verwendung mehrerer Prozesse kann unter Windows zu Problemen in der Bibliothek gensim führen.
     )
     coherence_score = coherence_model.get_coherence()
     return topics, coherence_score
 
-def test_topic_numbers(model_class, matrix, feature_names, clean_texts, topic_numbers=range(2,16)):
+def test_topic_numbers(model_class, matrix, feature_names, clean_texts, topic_numbers=range(2, 16),print_scores=False):
     '''
-
-    :param model_class: "LatentDirichletAllocation" oder "TruncatedSVD"
-    :param matrix: bow_matrix oder tfidf_matrix
-    :param feature_names: Themennamen
-    :param clean_texts: bereinigte Texte
-    :param topic_numbers: zu prüfende Themenzahlen (default 2-16 Themen)
-    :return:
+    Testet verschiedene Themenanzahlen für ein LSA- oder LDA-Modell
+    :param model_class: Modellklasse "LatentDirichletAllocation" oder "TruncatedSVD"
+    :param matrix: Vektorisierte Texte (bow_matrix oder tfidf_matrix)
+    :param feature_names: Wörter des verwendeten Vektorisierers
+    :param clean_texts: Bereinigte Texte
+    :param topic_numbers: zu testende Themenzahlen (default 2-16 Themen)
+    :param print_scores: Gibt die einzelnen Coherence Scores aus, wenn True
+    :return: Themenanzahl und zugehörige Coherence Scores
     '''
     coherence_scores = []
     for topic_number in topic_numbers:
-        model = model_class(n_components=topic_number, random_state=42) # random_state = 42 für reproduzierbare Ergebnisse
+        model = model_class(n_components=topic_number,
+                            random_state=42)  # random_state = 42 für reproduzierbare Ergebnisse
         model.fit(matrix)
-        topics, coherence_score = get_topics_coherence(model, feature_names, clean_texts, number_of_words)
+        topics, coherence_score = get_topics_coherence(model, feature_names, clean_texts, NUMBER_OF_WORDS)
         coherence_scores.append(coherence_score)
 
-        print(
-            f"{topic_number} Themen: "
-            f"Coherence Score = {coherence_score:.4f}"
-        )
+        if print_scores:
+            print(
+                f"{topic_number} Themen: "
+                f"Coherence Score = {coherence_score:.4f}"
+            )
     return list(topic_numbers), coherence_scores
-#LSA
-#lsa_topics, lsa_coherence = get_topics_coherence(lsa_model, tfidf_vectorizer.get_feature_names_out(), datafile["issue_description"], number_of_words)
-#print(lsa_topics)
-#print(lsa_coherence)
-print("LSA:")
+
+
+
+# ==========================================================
+# Datensatz laden
+# ==========================================================
+
+# CSV einlesen
+datafile = pd.read_csv(DATA_PATH)
+
+# Debuging:
+# behalten, falls der Datensatz geändert wird
+#print("Erste Zeilen:")
+#print(datafile.head())
+#print("Spalten:")
+#print(datafile.columns)
+#print("Größe:")
+#print(datafile.shape)
+#print("Null-Felder")
+#print(datafile.isnull().sum())
+# Spalte mit Beschwerdetexten auswählen:
+#texts = datafile["issue_description"]
+# Erste Beschwerdetexte anzeigen:
+#print("Beispieltexte:")
+#print(texts.head())
+
+#===========================================================
+# Texte bereinigen
+# ==========================================================
+
+#Stoppwörter und Lemmatizer vorbereiten
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+datafile["clean_description"] = datafile["issue_description"].apply(clean_text)
+
+# =========================================================
+# Texte vektorisieren
+# ==========================================================
+
+# BOW
+bow_vectorizer = CountVectorizer()
+bow_matrix = bow_vectorizer.fit_transform(datafile["clean_description"])
+
+# TF-IDF
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform(datafile["clean_description"])
+
+# ==========================================================
+# Coherence Scores je Themenanzahl bestimmen
+# ==========================================================
+
+# LSA
+if PRINT_SCORES:
+    print("LSA:")
 lsa_topic_numbers, lsa_coherence_scores = test_topic_numbers(
     TruncatedSVD,
     tfidf_matrix,
     tfidf_vectorizer.get_feature_names_out(),
-    datafile["clean_description"]
+    datafile["clean_description"],
+    TOPIC_NUMBERS,
+    PRINT_SCORES
 )
 
-#LDA
-#lda_topics, lsa_coherence = get_topics_coherence(lda_model, bow_vectorizer.get_feature_names_out(), datafile["issue_description"], number_of_words)
-#print(lda_topics)
-#print(lda_coherence)
-print("LDA:")
+# LDA
+if PRINT_SCORES:
+    print("LDA:")
 lda_topic_numbers, lda_coherence_scores = test_topic_numbers(
     LatentDirichletAllocation,
     bow_matrix,
     bow_vectorizer.get_feature_names_out(),
-    datafile["clean_description"]
+    datafile["clean_description"],
+    TOPIC_NUMBERS,
+    PRINT_SCORES
 )
 
+# ==========================================================
+# grafische darstellung der CS der Themenanzahlen von LSA und LDA
+# ==========================================================
 
-# grafische darstellung der SC der Themenanzahlen von LSA und LDA
 plt.plot(lsa_topic_numbers, lsa_coherence_scores, marker="o", label="LSA")
 plt.plot(lda_topic_numbers, lda_coherence_scores, marker="o", label="LDA")
 
@@ -262,14 +235,20 @@ plt.legend()
 plt.title("Coherence Score von LSA und LDA")
 plt.show()
 
+# ==========================================================
 # beste Themenanzahl für LSA und LDA auswählen
+# ==========================================================
+
 best_lsa_topics = lsa_topic_numbers[lsa_coherence_scores.index(max(lsa_coherence_scores))]
 best_lda_topics = lsa_topic_numbers[lda_coherence_scores.index(max(lda_coherence_scores))]
 print("Best Themenanzahl LSA:", best_lsa_topics)
 print("Best Themenanzahl LDA:", best_lda_topics)
 
-# 8 - LSA und LDA vergleichen
+# ==========================================================
+# Finale Modelle trainieren/erstellen
+# ==========================================================
 
+# LSA
 final_lsa_model = TruncatedSVD(n_components=best_lsa_topics, random_state=42)
 final_lsa_model.fit(tfidf_matrix)
 
@@ -277,10 +256,10 @@ final_lsa_topics, final_lsa_coherence = get_topics_coherence(
     final_lsa_model,
     tfidf_vectorizer.get_feature_names_out(),
     datafile["clean_description"],
-    number_of_words
+    NUMBER_OF_WORDS
 )
 
-
+# LDA
 final_lda_model = LatentDirichletAllocation(n_components=best_lda_topics, random_state=42)
 final_lda_model.fit(bow_matrix)
 
@@ -288,17 +267,17 @@ final_lda_topics, final_lda_coherence = get_topics_coherence(
     final_lda_model,
     bow_vectorizer.get_feature_names_out(),
     datafile["clean_description"],
-    number_of_words
+    NUMBER_OF_WORDS
 )
 
-# 9 - Ergebnis ausgeben
+# ==========================================================
+# Ergebnis ausgeben
+# ==========================================================
 
 print("\n--- LSA-Themen ---")
-for index, topic in enumerate(final_lsa_topics):
-    print(f"{index + 1}: {', '.join(topic)}")
+print_topics(final_lsa_topics)
 print(f"Coherence Score: {final_lsa_coherence:.4f}")
 
 print("\n--- LDA-Themen ---")
-for index, topic in enumerate(final_lda_topics):
-    print(f"{index + 1}: {', '.join(topic)}")
+print_topics(final_lda_topics)
 print(f"Coherence Score: {final_lda_coherence:.4f}")
